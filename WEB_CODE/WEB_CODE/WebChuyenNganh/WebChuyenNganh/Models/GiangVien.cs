09c14ae4ccb10df23292ee6f33eab92b464695f1
TuyetMi <luongtuyetmi456@gmail.com>
2024-06-28 19:48:05 +0700

    Revert "update"
    This reverts commit 1e6d7f17d58bdea2d461a6f63b608f4b904682b7.

--- WEB_CODE/WEB_CODE/WebChuyenNganh/WebChuyenNganh/Models/GiangVien.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebChuyenNganh.Models
-{
-    public class GiangVien
-    {
-        public string taikhoan { get; set; }
-        public string HoTenGV { get; set; }
-        public string Password { get; set; }
-    }
-}
